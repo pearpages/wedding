682d04f131ce72a283db203b7edd00a5e60fb414
pearpages <pere@soms.cat>
2020-02-29 19:59:09 +0100

    refactor: countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
-class Countdown extends Component {
-  render() {
-    return (
-      <div className="countdown_container">
-        <h2 className="centered_title">Countdown to Wedding Ceremony</h2>
-        <div id="defaultCountdown"></div>
-        <div className="rsvp_button">
-          <span className="swirl_left_small">
-            <span className="swirl_right_small">
-              <a href="rsvp.html">RSVP</a>
-            </span>
+import TextContext from "../core/TextContext";
+
+function Countdown() {
+  const translations: { [key: string]: any } = useContext(TextContext);
+  const texts = translations.countdown;
+  return (
+    <div className="countdown_container">
+      <h2 className="centered_title">{texts.title}</h2>
+      <div id="defaultCountdown"></div>
+      <div className="rsvp_button">
+        <span className="swirl_left_small">
+          <span className="swirl_right_small">
+            <Link to="/">{texts.button}</Link>
           </span>
-        </div>
+        </span>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Countdown;
