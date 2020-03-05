e0041d093d37235d514820bace3237c27fa7a3c3
pearpages <pere@soms.cat>
2020-03-05 02:32:11 +0100

    wip

--- src/pages/RSVP.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { t } from "helpers";
-import { Page, MOD } from "components";
-
-export function RSVP() {
-  return <Page title={t("rsvp.title")} mod={MOD.MOD2}></Page>;
-}
---
+++ src/pages/RSVP/Form.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+export function Form() {
+  const [hasPlusOne, setPlusOne] = useState(false);
+  return (
+    <div>
+      <div className="form_section">
+        <div className="form_row left12_first">
+          <label>
+            NOMBRE Y APELLIDOS<span>*</span>
+          </label>
+          <input
+            style={{ height: "48px" }}
+            type="text"
+            className="form_input required"
+            name="rsvpname"
+            id="rsvpname"
+          />
+        </div>
+        <div className="form_row left13_last">
+          <label>
+            ACOMPAÑANTE<span>*</span>
+          </label>
+          <div className="select_container">
+            <select
+              required
+              className="form_select"
+              name="desert"
+              onChange={e => {
+                setPlusOne(e.target.value === "SI");
+              }}
+            >
+              <option value="">--SELECCIONA--</option>
+              <option value="NO">NO</option>
+              <option value="SI">SI</option>
+            </select>
+          </div>
+        </div>
+        {hasPlusOne ? (
+          <div className="form_row left12_first">
+            <label>
+              NOMBRE Y APELLIDOS DEL ACOMPAÑANTE<span>*</span>
+            </label>
+            <input
+              style={{ height: "48px" }}
+              type="text"
+              className="form_input required"
+              name="rsvpname"
+              id="rsvpname"
+              required={hasPlusOne}
+            />
+          </div>
+        ) : null}
+
+        <div className="form_row_full">
+          <label>REQUERIMIENTOS ALIMENTARIOS</label>
+          <textarea
+            className="form_textarea_full"
+            name="menunotes"
+            style={{ height: "90px" }}
+          ></textarea>
+        </div>
+        <div className="form_row_full">
+          <label>OTROS COMENTARIOS</label>
+          <textarea
+            className="form_textarea_full"
+            name="rsvpcomments"
+            id="rsvpcomments"
+            style={{ height: "90px" }}
+          ></textarea>
+        </div>
+        <div className="form_row">
+          <button
+            style={{
+              width: "308px",
+              height: "54px",
+              border: "1px solid",
+              textAlign: "center"
+            }}
+            type="submit"
+            className="form_submit_contact"
+            id="submit"
+            // disabled={isPosting}
+          >
+            {/* {isPosting ? (
+              <img
+                style={{ display: "inline-block" }}
+                src="images/loader.gif"
+                alt="Loading..."
+                id="LoadingGraphic"
+              />
+            ) : ( */}
+            CONFIRMO
+            {/* )} */}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
