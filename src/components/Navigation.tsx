f3c53580d41419b2eed11cd8389dc58865b23468
pearpages <pere@soms.cat>
2020-03-08 11:34:15 +0100

    refactor: remove menu scripts

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import { appendScriptInTheDOM } from "helpers";
 import { Route } from "types";
 import { ROUTE_MAPPING } from "Routes";
 
 export function Navigation() {
-  const done = setInterval(() => {
-    try {
-      appendScriptInTheDOM("/js/menu.js");
-      appendScriptInTheDOM("/js/init2.js");
-      $(".show_menu").click(function() {
-        $(".menu").fadeIn();
-        $(".show_menu").fadeOut();
-        $(".hide_menu").fadeIn();
-      });
-      $(".hide_menu").click(function() {
-        $(".menu").fadeOut();
-        $(".show_menu").fadeIn();
-        $(".hide_menu").fadeOut();
-      });
-
-      clearInterval(done);
-    } catch (_) {}
-  }, 100);
   return (
     <>
       <span className="show_menu">
